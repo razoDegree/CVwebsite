fc58e80013fd93bcb56bfff82fe21bdc05fa7e3f
Raz Katz <degreerazo@gmail.com>
2025-06-08 17:00:20 +0300

    Connect the website to google analytics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,27 +11,51 @@ import WorkHistory from './WorkHistory';
 import Education from './Education';
 import Carousel from './Carousel';
 import MakeContact from './MakeContact';
+import { useEffect } from 'react'
 
 function App() {
+  useEffect(() => {
+    const GA_ID = import.meta.env.VITE_GOOGLE_ANALYTICS_ID
+    const isProd = import.meta.env.MODE === 'production'
 
-  return (
-    <>
-      <CustomCursor>
-        <FloatingMemoji />
-        <SmoothScroll>
-          <Header />
-          <LandingContiner />
-          <Video />
-          <ShortDescription />
-          <ProjectSection />
-          <WorkHistory />
-          <Education />
-          <Carousel />
-          <MakeContact />
-        </SmoothScroll>
-      </CustomCursor>
-    </>
-  );
+    if (!GA_ID || !isProd) return
+
+    const script1 = document.createElement('script')
+    script1.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`
+    script1.async = true
+    document.head.appendChild(script1)
+
+    const script2 = document.createElement('script')
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${GA_ID}');
+    `
+    document.head.appendChild(script2)
+  }, [])
+
+  function App() {
+
+    return (
+      <>
+        <CustomCursor>
+          <FloatingMemoji />
+          <SmoothScroll>
+            <Header />
+            <LandingContiner />
+            <Video />
+            <ShortDescription />
+            <ProjectSection />
+            <WorkHistory />
+            <Education />
+            <Carousel />
+            <MakeContact />
+          </SmoothScroll>
+        </CustomCursor>
+      </>
+    );
+  }
 }
 
 export default App;
